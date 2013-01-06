bd682c8f6944ad6875c7d8d481618e814cba5d03
David Lesches <david@lesches.com>
2013-01-05 21:58:18 -0500

    Adding LinkedIn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,25 +5,20 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, APP_CONFIG['fb_app_id'], APP_CONFIG['fb_secret'], {
-    :client_options => {
-      :ssl => APP_CONFIG["ssl_certs"]
-    },
+    :client_options => { :ssl => APP_CONFIG["ssl_certs"] },
     :scope => APP_CONFIG["fb_permissions"]
   }
 
   provider :foursquare, APP_CONFIG['foursquare_id'], APP_CONFIG['foursquare_secret'], {
-    :client_options => {
-      :ssl => APP_CONFIG["ssl_certs"]
-    }
+    :client_options => { :ssl => APP_CONFIG["ssl_certs"] }
   }
 
   provider :twitter, APP_CONFIG['twitter_id'], APP_CONFIG['twitter_secret'], {
-    :client_options => {
-      :ssl => APP_CONFIG["ssl_certs"]
-    }
+    :client_options => { :ssl => APP_CONFIG["ssl_certs"] }
   }
+
+  provider :linkedin, APP_CONFIG['linkedin_id'], APP_CONFIG['linkedin_secret'], :scope => APP_CONFIG["linkedin_permissions"]
+
 end
 
-Koala.http_service.http_options = {
-  :ssl => { :verify => false }
-}
+Koala.http_service.http_options = { :ssl => { :verify => false } }
